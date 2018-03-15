84c5ef2e92772a969212fff585ec26c48121cc5c
Roman <roman.leonhardt@zamg.ac.at>
2018-03-15 14:29:31 +0000

    updating pos1init

--- init/pos1init.sh
+++ init/pos1init.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 PYTHONPATH="/home/cobs/anaconda2/bin/python"
+MARTASPATH="/home/cobs/MARTAS"
 
-$PYTHONPATH serialinit.py -p "/dev/ttyS0" -b 9600 -c "mode text,time datetime,date 11-22-16,range 48500,auto 5" -k "%H:%M:%S" -x -d 0
+$PYTHONPATH $MARTASPATH/app/serialinit.py -p "/dev/ttyS0" -b 9600 -c "mode text,time datetime,date 11-22-16,range 48500,auto 5" -k "%H:%M:%S" -x -d 0
