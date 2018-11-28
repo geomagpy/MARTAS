098a296a02799c0270fcb030af5ee729ede5e829
Roman Leonhardt <leon@saturn.zamg.ac.at>
2018-11-28 17:59:46 +0000

    changed lorazamg

--- libmqtt/lorazamg.py
+++ libmqtt/lorazamg.py
@@ -17,6 +17,27 @@ def datetime2array(t):
 ##
 class lorazamg(object):
     """
+    ZAMG/adeunis {"DateTime":"2018-11-28T16:17:47.517412Z","Modell":"Adeunis RF","Name":"Adeunis7 - Museumsstr","issue":{"appeui":"00-18-b2-43-45-4e-53-33","deveui":"00-18-b2-20-00-00-02-6b","code":66,"status_nr":224,"channelA mA":11.09218,"channelB mA":11.09218,"status":0},"data":{"corr":0,"tl":14.326125000000005,"rf":41.194874999999996,"msg_payload":[66,224,18,16,236,226,34,16,41,46]}}
+
+    ZAMG/bees {"DatumSec":"2018-11-28T17:02:59.352375Z","Name":"Unknown bees","issue":{"appeui":"48-83-c7-df-30-0d-00-00","deveui":"48-83-c7-df-30-0d-12-74","breite":48.2483,"laenge":16.357428,"hoehe":198,"st":0},"data":{"tl":21.75,"rf":36.9,"saved_payload":[1,127,8,106,14,92,0]}}
+
+    ZAMG/ikt/isd/test/jf/adeunis07_beelike {"DateTime":"2018-11-28T16:17:47.517412Z","deveui":"00-18-b2-20-00-00-02-6b","breite":48.2483,"laenge":16.357428,"hoehe":148,"status":0,"tl":14.326125000000005,"rf":41.194874999999996}
+
+
+
+    content suggestion: appeui, deveui, sensorname, locationname, sensormodell, -> rest beelike
+
+    topic suggestions:
+    headline/station/sensor
+
+    ideally, headline is a unique formatidentifier
+    e.g.
+    loraz/schwarzenbergplatz/adeunis  od 
+    warum: so kann man relativ systematisch stationen abfragen
+    mobile sensoren ohne festen standort:
+    loraz/mobile/adeunis
+
+
     """
 
     def __init__(self):
@@ -25,13 +46,14 @@ class lorazamg(object):
         print ("  -> Initializing lora zamg routines ...")
         #self.payload = payload
         #self.topic = topic
-        self.topicidentifier = {'startswith':'ZAMG','endswith':'adeunis'}
+        self.topicidentifier = {'startswith':'ZAMG','endswith':'s'}
         self.datakeytranslator = {'tl':['t1','degC'], 'rf':['var1','per'], 'corr':['var5','none']}
         self.identifier = {}
         self.headdict = {}
         self.headstream = {}
 
     def GetPayload(self, payload, topic):
+        print (topic)
         loradict = json.loads(payload)
         # convert loradict to headdict (header) and data_bin
         newpayload, sensorid, headline, header =  self.loradict2datastruct(loradict)
@@ -45,6 +67,7 @@ class lorazamg(object):
             header['SensorName'] = loradict.get('Name').split(' ')[0]
             try:
                 header['StationID'] = loradict.get('Name').strip().split(' - ')[1]
+                header['StationName'] = loradict.get('Name').strip().split(' - ')[1]
             except:
                 header['StationID'] = "Not defined"
             #header['SensorName'] = loradict.get('Name').split(' - ').strip()[0]
@@ -53,6 +76,13 @@ class lorazamg(object):
             header['SensorGroup'] = loradict.get('Modell')
             sensorid = header['SensorName'].split(' ')[0] + '_' + header['SensorSerialNum'] + '_0001'
             header['SensorID'] = sensorid
+            header['StationLongitude'] = issuedict.get('laenge','')
+            header['StationLatitude'] = issuedict.get('breite','')
+            if issuedict.get('laenge',''):
+                header['StationLocationReference'] = 'WGS84, EPSG: 4326'
+            header['StationElevation'] = issuedict.get('hoehe','')
+            if issuedict.get('hoehe',''):
+                header['StationElevationRef'] = issuedict.get('hoehe','')
 
             # needs to return headstream[sensorid] = header (global)
             # identifier dictionary  (global)
@@ -61,7 +91,10 @@ class lorazamg(object):
             # and data payload
 
             keylist, elemlist, unitlist, multilist = [],[],[],[]
-            time = datetime.strptime(loradict.get('DateTime'),"%Y-%m-%dT%H:%M:%S.%fZ")
+            if loradict.get('DateTime',''):
+                time = datetime.strptime(loradict.get('DateTime'),"%Y-%m-%dT%H:%M:%S.%fZ")
+            elif loradict.get('DatumSec',''):
+                time = datetime.strptime(loradict.get('DateTime'),"%Y-%m-%dT%H:%M:%S.%fZ")
             datalst = datetime2array(time)
             packstr = '6hL'
             for elem in datadict:
