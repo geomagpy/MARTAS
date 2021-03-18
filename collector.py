4333e47f0de11a2c4178e4ccf80f84a56c60810d
leonro <roman.leonhardt@zamg.ac.at>
2021-03-18 17:48:02 +0100

    added blacklist to collector in order to NOT collect data of specified sensors

--- collector.py
+++ collector.py
@@ -123,7 +123,7 @@ stid = stationid
 webpath = './web'
 webport = 8080
 socketport = 5000
-
+blacklist = []
 
 class protocolparameter(object):
     def __init__(self):
@@ -362,6 +362,11 @@ def on_message(client, userdata, msg):
     if not instrument == '':
         if not sensorid.find(instrument) > -1:
             return
+
+    if sensorid in blacklist:
+        if debug:
+            print ("Sensor {} in blacklist - not collecting".format(sensorid))
+        return
 
     ## ################################################################################
     ## ####            Eventually check for additional format libraries       #########
@@ -719,6 +724,8 @@ def main(argv):
     number=1
     global qos
     qos=0
+    global blacklist
+    blacklist = []
 
     usagestring = 'collector.py -b <broker> -p <port> -t <timeout> -o <topic> -i <instrument> -d <destination> -v <revision> -l <location> -c <credentials> -r <dbcred> -q <qos> -u <user> -P <password> -s <source> -f <offset> -m <marcos> -n <number> -e <telegramconf> -a <addlib>'
     try:
@@ -814,6 +821,9 @@ def main(argv):
                     qos = 0
             if not conf.get('mqttcredentials','') in ['','-']:
                 credentials=conf.get('mqttcredentials').strip()
+            if not conf.get('blacklist','') in ['','-']:
+                blacklist=conf.get('blacklist').split(',')
+                blacklist = [el.strip() for el in blacklist]
             if not conf.get('station','') in ['','-']:
                 stationid = conf.get('station').strip()
                 stid = stationid
