5a7fa482ed1f8de695048a8192da778b2cf57658
rmandl <richard.mandl@zamg.ac.at>
2021-12-09 17:36:56 +0100

    enable obsdaqinit.sh to use config fiole or not

--- init/obsdaqinit.sh
+++ init/obsdaqinit.sh
@@ -3,36 +3,38 @@
 PYTHON="/usr/bin/python"
 MARTASPATH="/home/pi/MARTAS"
 APPPATH=$MARTASPATH"/app"
-CONF="/etc/martas/obsdaq.cfg"
+# run without config file: CONF="" otherwise CONF="-m /config/file"
+#CONF=""
+CONF="-m /etc/martas/obsdaq.cfg"
 
 # set PalmAcq into idle mode (command mode)
-$PYTHON $APPPATH/palmacq.py -m $CONF -qp
+$PYTHON $APPPATH/palmacq.py $CONF -qp
 sleep 2
-$PYTHON $APPPATH/palmacq.py -m $CONF -qp
+$PYTHON $APPPATH/palmacq.py $CONF -qp
 sleep 2
 # wait for GPS loopseconds=18
-$PYTHON $APPPATH/palmacq.py -m $CONF -qg
+$PYTHON $APPPATH/palmacq.py $CONF -qg
 
 # set PalmAcq into Transparent mode to access ObsDAQ
-$PYTHON $APPPATH/palmacq.py -m $CONF -qt
+$PYTHON $APPPATH/palmacq.py $CONF -qt
 sleep 2
 # stop ObsDAQ's acquisition, if running  
-$PYTHON $APPPATH/obsdaq.py -m $CONF -qp
+$PYTHON $APPPATH/obsdaq.py $CONF -qp
 sleep 5
-$PYTHON $APPPATH/obsdaq.py -m $CONF -qp
+$PYTHON $APPPATH/obsdaq.py $CONF -qp
 sleep 2
 # print serial number
-$PYTHON $APPPATH/obsdaq.py -m $CONF -qi
+$PYTHON $APPPATH/obsdaq.py $CONF -qi
 # execute a self calibration resp. load calibration values
-$PYTHON $APPPATH/obsdaq.py -m $CONF -qc
+$PYTHON $APPPATH/obsdaq.py $CONF -qc
 sleep 2
 # start acquisition
-$PYTHON $APPPATH/obsdaq.py -m $CONF -qa
+$PYTHON $APPPATH/obsdaq.py $CONF -qa
 sleep 2
 # set PalmAcq into idle mode
-$PYTHON $APPPATH/palmacq.py -m $CONF -qp
+$PYTHON $APPPATH/palmacq.py $CONF -qp
 sleep 2
 # set PalmAcq into forward mode
-$PYTHON $APPPATH/palmacq.py -m $CONF -q -d R
+$PYTHON $APPPATH/palmacq.py $CONF -q -d R
 sleep 2
 
