cb43b3c61634cd92d79b409255782b4b571cc0a7
leonro <roman.leonhardt@geosphere.at@>
2025-07-17 22:55:46 +0200

    simple graph routine

--- martas/web/marcos.py
+++ martas/web/marcos.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python
 
-from dash import Dash, html
+from magpy.stream import *
+import pandas as pd
+from dash import Dash, html, dash_table, dcc
+import plotly.express as px
+
+
+def magpy2pandas(stream):
+    d = {}
+    for key in stream.KEYLIST:
+        col = stream._get_column(key)
+        colname = stream.header.get('col-{}'.format(key), key)
+        if len(col) > 0:
+            d[colname] = col
+    dataset = pd.DataFrame(d)
+    return dataset
+
+# read data
+data = read(example1)
+# convert to pandas
+df = magpy2pandas(data)
 
 app = Dash()
 
 # Requires Dash 2.17.0 or later
-app.layout = [html.Div(children='Hello World')]
+app.layout = [
+    html.Div(children='My First App with Data and a Graph'),
+    dcc.Graph(figure=px.line(df, x='time', y="H"))
+]
 
 if __name__ == '__main__':
     app.run(debug=True)
